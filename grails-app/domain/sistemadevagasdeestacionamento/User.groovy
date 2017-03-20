d37b9b5c2242cffda828901992c5554218fd18d2
Victor Souto <victorsoouto@gmail.com>
2017-03-20 18:38:18 -0300

    merge com os arquivos de Mateus

--- grails-app/domain/sistemadevagasdeestacionamento/User.groovy
+++ grails-app/domain/sistemadevagasdeestacionamento/User.groovy
@@ -8,8 +8,39 @@ class User {
 
     static constraints = {
         username nullable: false, blank: false, unique: true
-        firstName nullable: false, blank: false, matches: "[a-zA-Z]+"
-        lastName nullable: false, blank: false, matches: "[a-zA-Z]+"
-        preferredSector inList: ["CIn", "CCEN", "Area II"]
-    }
+		firstName nullable: false, blank: false, matches: "[a-zA-Z]+"
+		lastName nullable: false, blank: false, matches: "[a-zA-Z]+"
+		preferredSector inList: ["CIn", "CCEN", "Area II"]
+	} 
+	
+	@Override
+	public String toString(){
+		return "Primeiro nome: $firstName, Segundo nome: $lastName, Setor preferido: $preferredSector"
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
 }
