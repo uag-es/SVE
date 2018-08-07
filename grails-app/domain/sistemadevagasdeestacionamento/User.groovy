d6d37850d8269d3f93dc03594bc3584da5c7c67e
Danillo Bion <danillobion@gmail.com>
2018-08-07 05:15:32 -0800

    tres melhorias na qualidade do codigo - Codenarc

--- grails-app/domain/sistemadevagasdeestacionamento/User.groovy
+++ grails-app/domain/sistemadevagasdeestacionamento/User.groovy
@@ -8,10 +8,44 @@ class User {
 	String preferredSector
 
 	static constraints = {
-		username nullable: false, blank: false, unique: true
+		username nullable: false, blank: false, unique: true, size: 1..15
 		password nullable: false, blank: false
 		firstName nullable: false, blank: false, matches: "[A-Za-z]+"
 		lastName nullable: false, blank: false, matches: "[A-Za-z]+"
 		preferredSector inList: ["CIn", "CCEN", "Area II"]
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof User))
+			return false;
+		User other = (User) obj;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password
+				+ ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", preferredSector=" + preferredSector + "]";
+	}
+	
+	
 }
