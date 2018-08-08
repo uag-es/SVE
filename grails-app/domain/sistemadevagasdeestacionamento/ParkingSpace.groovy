9ba556de12ed641d407bdb5d8e7fa4b36e81a97c
Geovanio José da Silva <sgeojose@gmail.com>
2018-08-07 21:06:11 -0300

    Tres melhorias na classe ParkingSpace utilizando o Codenarc

--- grails-app/domain/sistemadevagasdeestacionamento/ParkingSpace.groovy
+++ grails-app/domain/sistemadevagasdeestacionamento/ParkingSpace.groovy
@@ -8,11 +8,43 @@ class ParkingSpace {
 
 	static constraints = {
 		owner nullable: true
-		description nullable: false, blank: false, unique: true
+		description nullable: false, blank: false, unique: true, size: 1..40
 		sector inList: ["CIn", "CCEN", "Area II"]
 	}
 
 	boolean isAvailable() {
 		return owner == null
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((owner == null) ? 0 : owner.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ParkingSpace other = (ParkingSpace) obj;
+		if (owner == null) {
+			if (other.owner != null)
+				return false;
+		} else if (!owner.equals(other.owner))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ParkingSpace [description=" + description + ", sector="
+				+ sector + ", preferential=" + preferential + "]";
+	}
+	
 }
